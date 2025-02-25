6a66e36f4b6530c8d7fba459ab3dafe45156e3b9
João Pedro R <162389145+Joao-Pedro-R@users.noreply.github.com>
2025-02-24 22:57:53 -0300

    Add files via upload

---
+++ 240225/src/annotation/Coluna.java
@@ -0,0 +1,18 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Define até quando a anotação estará presente (código fonte)
+@Retention(RetentionPolicy.RUNTIME)
+//Define onde pode ser utilizada a anotação(field-atributos)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Coluna {
+
+    String nome();
+    int tamanho() default 0;
+    boolean obrigatorio() default false;
+
+}
